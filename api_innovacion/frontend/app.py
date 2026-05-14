2fcc2a4f9ef7102cb82633e890bcf8721de565f9
jeronimo <jp.posada6@gmail.com>
2026-05-14 13:21:44 -0500

    Fix: agregar app.py al frontend

---
+++ api_innovacion/frontend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:filename>')
+def serve_file(filename):
+    return send_from_directory('.', filename)
